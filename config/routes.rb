ac97231988e06c53ed63b5abdd54989cfc15e3dd
Tim Graney <texasjunkcat@gmail.com>
2014-11-20 17:17:19 -0500

    added microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :microposts
+
+  resources :users
+
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +60,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
